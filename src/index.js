184a939c57aaadd3859cd0319c5422d36b962c53
Kwandes <bz.jasiek@gmail.com>
2021-06-17 21:06:06 +0200

    Add ability to apply custom hydra patterns via text input

--- src/index.js
+++ src/index.js
@@ -6,10 +6,34 @@ const hydra = new Hydra({
   detectAudio: false,
 });
 
+// hydra command to show if the new evaluated command fails
+let lastValidHydraCommand = "osc(10, 0.1, 0.8).out()";
+
 function init() {
   console.log("Time to start the show!");
   console.dir(canvas);
 
   console.log("Starting the default light show");
-  osc(10, 0.1, 0.8).rotate(0, 0.1).kaleid().color(-1, 1).out();
+  // Apply the default hydra command
+  evaluateHydraCommand(lastValidHydraCommand);
+
+  // read the command input and apply it to the canvas if it is valid
+  const commandInput = document.getElementById("command-input");
+  commandInput.addEventListener("input", () => {
+    evaluateHydraCommand(commandInput.value);
+  });
+}
+
+// attempt to execute a given hydra command
+function evaluateHydraCommand(command) {
+  try {
+    eval(command);
+    lastValidHydraCommand = command;
+  } catch (exception) {
+    console.log("Provided command is not a valid hydra string");
+    //console.log(exception);
+    eval(lastValidHydraCommand);
+    return false;
+  }
+  return true;
 }
