ac0d3ac87f85d0a981f61ddff48a71f52b736059
Kwandes <bz.jasiek@gmail.com>
2021-06-24 22:37:51 +0200

    Add ability for pattern type to change on beat of the music

--- src/audio.js
+++ src/audio.js
@@ -6,13 +6,58 @@ console.log(audio);
 audio.addEventListener("play", startListeningToAudio);
 audio.addEventListener("pause", stopListeningToAudio);
 
+let audioPatternLoopInterval;
+
+const patternChangeMinFrequencyPerMinute = 4; // Pattern will change at least X times per minute
+const patternChangeCheckFrequency = 60; // How often to check the pattern change, per second
+const minimumTimeBetweenPatternChanges = 5; // seconds
+let intervalCounter = 0;
+
 function startListeningToAudio() {
   console.log("Listening to your bullshit");
   a.show();
+  audioPatternLoopInterval = setInterval(() => {
+    intervalCounter++;
+    // don't change the pattern if it changed too often
+    if (
+      (minimumTimeBetweenPatternChanges * 1000) / patternChangeCheckFrequency >
+      intervalCounter
+    ) {
+      return;
+    }
+    // Only change the pattern on loud beats or if it hasn't changed in X seconds
+    if (Math.floor(a.fft[0] * 100) % 10 < 7) {
+      if (intervalCounter != 1000 / patternChangeMinFrequencyPerMinute) {
+        return;
+      }
+    }
+    intervalCounter = 0;
+
+    let backgroundPatternIndex =
+      Math.floor(a.fft[0] * 100) % backgroundPattern.length;
+    let foregroundPatternIndex =
+      Math.floor(a.fft[0] * 100) % foregroundPattern.length;
+    console.log(
+      `Pattern change, new pattern is background[${backgroundPatternIndex}] and foreground[${foregroundPatternIndex}]`
+    );
+
+    const newPattern =
+      commandStart +
+      backgroundPattern[Math.floor(Math.random() * backgroundPattern.length)] +
+      scrollPattern +
+      ".add(" +
+      //commandStart +
+      foregroundPattern[Math.floor(Math.random() * foregroundPattern.length)] +
+      ").out();";
+    evaluateHydraCommand(newPattern);
+    document.getElementById("command-input").innerText = newPattern;
+  }, 1000 / patternChangeCheckFrequency);
 }
 
 function stopListeningToAudio() {
   console.log("I'm done with your bullshit");
   a.hide();
+  clearInterval(audioPatternLoopInterval);
 }
+
 //() => (a.fft[0]*4)
